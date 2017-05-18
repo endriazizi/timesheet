2f58783e87fab9982474e7230beb308c4ba100b4
AZIZ ENDRI <U0I6910@sede.corp.sanpaoloimi.com>
2017-05-18 17:31:15 +0200

    initial code comments, explaining main.js and index.html how they works with vue.js code

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 new Vue({
+  //id element goes to show up into index page between div tags,
+  //el stands for element, ex:   <div id="app"></div>
   el: '#app',
+  //for rendereing the page into index.html page view
   render: h => h(App)
 })
